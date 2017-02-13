88c583c7379381ccc7dc4f3a521ab15002caabf9
Sceat <mr.sceat@outlook.com>
2017-02-13 16:36:48 +0100

    :zap: enhancing percent printer

--- src/main/java/fr/aresrpg/tofumanchou/domain/util/PercentPrinter.java
+++ src/main/java/fr/aresrpg/tofumanchou/domain/util/PercentPrinter.java
@@ -11,7 +11,7 @@ public class PercentPrinter {
 	public static final String FORMAT = "[%s%%]";
 	private final int total;
 	private int current;
-	private int lastPercent;
+	private int last;
 
 	public PercentPrinter(int total) {
 		this.total = total;
@@ -24,35 +24,9 @@ public class PercentPrinter {
 	public void incrementAndPrint(String format) {
 		++current;
 		int percent = getPercent();
-		if (percent < 10) percent = 1;
-		else if (percent < 20) percent = 10;
-		else if (percent < 30) percent = 20;
-		else if (percent < 40) percent = 30;
-		else if (percent < 50) percent = 40;
-		else if (percent < 60) percent = 50;
-		else if (percent < 70) percent = 60;
-		else if (percent < 80) percent = 70;
-		else if (percent < 90) percent = 80;
-		else if (percent < 100) percent = 90;
-		else percent = 100;
-		if (percent <= lastPercent) return;
-		switch (percent) {
-			case 1:
-			case 10:
-			case 20:
-			case 30:
-			case 40:
-			case 50:
-			case 60:
-			case 70:
-			case 80:
-			case 90:
-			case 100:
-				lastPercent = percent;
-				LOGGER.info(String.format(format, percent));
-				break;
-			default:
-				break;
+		if (last < percent && percent / 10 * 10 == percent) {
+			last = percent;
+			LOGGER.info(String.format(format, percent));
 		}
 	}
 
@@ -65,6 +39,7 @@ public class PercentPrinter {
 
 	@Override
 	public String toString() {
-		return "PercentPrinter [total=" + total + ", current=" + current + ", lastPercent=" + lastPercent + "]";
+		return "PercentPrinter [total=" + total + ", current=" + current + "]";
 	}
+
 }
